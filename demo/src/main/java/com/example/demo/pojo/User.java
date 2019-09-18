143de2b31b7ee1e7a68cd24bc6230e2771abdac1
fancao <247919449@qq.com>
2019-09-18 11:01:56 +0800

    test

---
+++ demo/src/main/java/com/example/demo/pojo/User.java
@@ -0,0 +1,31 @@
+package com.example.demo.pojo;
+
+public class User {
+    private String name;
+    private Integer age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
